09dcfe6b1a5446d5fb499745fbb6a534eab14671
Almheb6 <102483123+Almheb6@users.noreply.github.com>
2022-06-22 10:01:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import setuptools
-from HamodyToolp import Hamody
+import HamodyToolp import Hamody
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-with open('requirements.txt','r') as fr:
-    requires = fr.read().split('\n')
+with open("README.md", "r", encoding="utf-8") as fh: long_description = fh.read()
 
 setuptools.setup(
     name="HamodyToolp",
